75d663426d97b9b8dd9e8fb70b40c9ce27a8105d
stevewhitmore <s25w89@gmail.com>
2022-11-07 10:24:37 -0600

    Started working on python logic

--- scripts/ci.py
+++ scripts/ci.py
@@ -1,5 +1,36 @@
 """Holds logic for pipeline operations"""
+import os
+import sys
+from git import Repo
 
 def get_diffs():
     """Gets the git diffs to determine which libraries to run operations on"""
-    print('does a thing')
+    path = os.getcwd()
+    repo = Repo(path)
+    repo.remotes.origin.fetch()
+    diff = str(repo.git.diff('origin/main', name_only=True)).splitlines()
+
+    if len(diff) != 0:
+        print(diff)
+
+
+def eslint():
+    """Runs eslint on libraries with changes"""
+    print("running eslint...")
+
+
+def unit_tests():
+    """Runs unit tests on libraries with changes"""
+    print("running unit tests...")
+
+
+def publish_snapshots():
+    """Publishes npm snapshots on libraries with changes"""
+    print("Publishes npm snapshots...")
+
+
+if len(sys.argv) < 2:
+    print("You must call this script with an argument (e.g. `python scripts/ci.py eslint`)")
+    sys.exit(1)
+
+locals()[sys.argv[1]]()
